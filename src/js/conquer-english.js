11f45e25214c7d15acc1b13d1f3c97e7caf6dd6d
Álvaro Barrena <alvarobarrena.dev@gmail.com>
2025-08-02 18:08:45 +0200

    Finished responsive design for Slider section on Conquer English page

---
+++ src/js/conquer-english.js
@@ -0,0 +1,29 @@
+import Swiper from "swiper";
+import { Navigation, Pagination, Autoplay, EffectFade } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/effect-fade";
+
+Swiper.use([Navigation, Pagination, Autoplay, EffectFade]);
+
+Swiper.use([
+  
+]);
+
+new Swiper(".conquer_english__hero", {
+  loop: true,
+  autoplay: {
+    delay: 5000,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  effect: "fade",
+  speed: 800,
+});
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,4 @@
 import "@sass/main.scss";
 import "./home.js";
 import "./lenis.js";
+import "./conquer-english.js";
