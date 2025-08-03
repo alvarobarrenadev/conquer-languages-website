77985ec3fb970b1b816e6dbd06ee6634ccfdde39
Álvaro Barrena <alvarobarrena.dev@gmail.com>
2025-08-03 20:53:41 +0200

    Finished responsive design for Master section on Conquer English Page

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,23 @@ import "@sass/main.scss";
 import "./home.js";
 import "./lenis.js";
 import "./conquer-english.js";
+
+// Reproductor Plyr
+import 'plyr/dist/plyr.css'; // estilos por defecto de Plyr
+import Plyr from 'plyr'; // la librería
+
+// Espera a que esté el DOM listo
+document.addEventListener('DOMContentLoaded', () => {
+  // Inicializa todos los videos que estén dentro del wrapper
+  const selector = '.conquer_english__master--video video';
+  const players = Array.from(document.querySelectorAll(selector))
+    .map(video => new Plyr(video, {
+      controls: [
+        'play-large', // botón grande al centro
+        'play', 'progress', 'current-time',
+        'mute', 'volume', 'settings',
+        'fullscreen'
+      ],
+      settings: ['quality', 'speed', 'loop']
+    }));
+});
